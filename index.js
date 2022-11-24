f4984a01c3f6ef4e8c603eaf4af46eb90b51cb8e
Indra Kurniawan <114134339+indranaftena@users.noreply.github.com>
2022-11-24 01:49:59 +0000

    pass freecodecamp test and prevent files to be saved

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
-var app = express();
+const { unlink } = require('node:fs');
+
+const app = express();
 
 app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // delete file to prevent storage run out
+  unlink(req.file.path, (err) => {
+    if (err) throw err;
+    console.log(req.file.originalname, 'or', req.file.filename, 'is deleted');
+  });
+  // response
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
